26a889d10935d3a42929b69a5676748e02c26340
BOUSSEBATA ISSAM <li_boussebata@esi.dz>
2024-02-05 12:43:38 +0100

    update navbar landing

--- my-app/src/Components/Navbar_landing.js
+++ my-app/src/Components/Navbar_landing.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../Styles/Navbar.css";
+import { NavLink } from "react-router-dom";
 import { CiPhone } from "react-icons/ci";
 import { Link, useLocation } from "react-router-dom";
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
@@ -52,19 +53,17 @@ function Navbar_landing(params) {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
 
   return (
-
     <div className="flex  w-screen items-center justify-around h-20 bg-grey">
-      <div className="logo-igl flex items-center content-center">
-      <Link to="./">
-        <img className="logo-easy h-12" src="./Assets/logo.png" alt="" />
-      </Link>
-      <Link to="./">
-        <img className="logo-nom ml-2" src="./Assets/nom.png" alt="" />
-      </Link>
-    </div>
+      <div className="logo-igl flex   items-center content-center">
+        <NavLink to="/">
+          <img className="logo-easy  h-12" src="./Assets/logo.png" alt="" />
+        </NavLink>
+        <NavLink to="/">
+          <img className="logo-nom ml-2" src="./Assets/nom.png" alt="" />
+        </NavLink>
+      </div>
 
       <div className=" hidden md:flex  justify-between items-center object-none object-center ml-8 ">
         <ul className="flex gap-5">
@@ -80,7 +79,6 @@ function Navbar_landing(params) {
               Home
             </ScrollLink>
           </li>
-
           <li className="cursor-pointer hover:text-blue hover:underline">
             <ScrollLink
               to="team"
@@ -119,7 +117,7 @@ function Navbar_landing(params) {
       </div>
 
       {respoListVisible && (
-        <div className="respo-listL  grid justify-items-end content-center gap-5 mt-3 ">
+        <div className=" absolute grid right-5 top-2 justify-items-end content-center gap-5 mt-3 ">
           <div className=" rounded-3xl h-[200px] mt-16  grid mr-3 content-start gap-0 justify-items-center w-[250px] ">
             <ul className=" bg-grey grid gap-8  rounded-3xl pb-10  mr-8  content-start  justify-items-center w-[250px] ">
               <button href="" onClick={respoOff} className="icon-x">
@@ -127,7 +125,7 @@ function Navbar_landing(params) {
               </button>
               <li className="cursor-pointer hover:text-blue hover:underline">
                 <ScrollLink
-                  to="home"
+                  to="home_land"
                   smooth={true}
                   duration={500}
                   className="flex items-center gap-3"
@@ -138,7 +136,7 @@ function Navbar_landing(params) {
               </li>
               <li className="cursor-pointer hover:text-blue hover:underline">
                 <ScrollLink
-                  to="profile"
+                  to="team"
                   smooth={true}
                   duration={500}
                   className="flex items-center gap-3"
@@ -148,7 +146,7 @@ function Navbar_landing(params) {
               </li>
               <li className="cursor-pointer hover:text-blue hover:underline">
                 <ScrollLink
-                  to="favorites"
+                  to="contact"
                   smooth={true}
                   duration={500}
                   className="flex items-center gap-3"
